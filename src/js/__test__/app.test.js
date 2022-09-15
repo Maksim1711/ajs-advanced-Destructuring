8587cf5e6324243038e996a4bc9014037a4faec3
Maksim Oloviannikov <olovjanqnet@gmail.com>
2022-09-15 21:27:12 +0300

    added advanced-Destructuring

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,37 @@
+import attackExtractor from '../app';
+
+test('extract attacks 2', () => {
+	const character = {
+		special: [
+			{
+				id: 8,
+				name: 'Двойной выстрел',
+				icon: 'http://...',
+				description: 'Двойной выстрел наносит двойной урон',
+			},
+			{
+				id: 9,
+				name: 'Нокаутирующий удар',
+				icon: 'http://...',
+			},
+		],
+	};
+	const result = [
+		{
+			id: 8, name: 'Двойной выстрел', icon: 'http://...', description: 'Двойной выстрел наносит двойной урон',
+		},
+		{
+			id: 9, name: 'Нокаутирующий удар', icon: 'http://...', description: 'Описание недоступно',
+		},
+	];
+	expect(attackExtractor(character)).toEqual(result);
+});
+
+test('extract attacks', () => {
+	const character = {
+		special: [],
+	};
+	const result = [];
+	expect(attackExtractor(character)).toEqual(result);
+});
+
